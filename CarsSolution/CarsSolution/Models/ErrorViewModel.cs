467c02e4cf73169c48f666f70e64cfc16908edcc
AndriKls <158137294+AndriKls@users.noreply.github.com>
2024-12-13 00:37:01 +0200

    Initial commit

---
+++ CarsSolution/CarsSolution/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarsSolution.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
